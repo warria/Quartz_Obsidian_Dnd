89b1b17a8fe35b29fcf090fa39802833c46a80d0
Your Name <you@example.com>
2025-10-06 12:52:08 +0300

    Quartz sync: Oct 6, 2025, 12:52 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Welcome to Jaconia",
+    pageTitle: "Dnd Jaconia",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
